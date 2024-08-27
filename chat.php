6df0801491e651a3b3593bfe285bf2c000e2aa66
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 15:11:31 +0300

    chat area

---
+++ chat.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+include_once "php/config.php";
+if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+}
+?>
+<?php include_once "header.php";?>
+<body>
+  <div class="wrapper">
+    <section class="chat-area">
+      <header>
