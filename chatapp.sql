3d1e342890cfb727ff2052011a31b537055c2908
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 15:25:18 +0300

    create table users

--- chatapp.sql
+++ chatapp.sql
@@ -6,36 +6,43 @@
 -- Generation Time: Feb 18, 2021 at 11:44 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.3.26
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!40101 SET NAMES utf8mb4 */
+;
 --
 -- Database: `chatapp`
 --
 
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `messages`
 --
 
 CREATE TABLE `messages` (
-  `msg_id` int(11) NOT NULL,
-  `incoming_msg_id` int(255) NOT NULL,
-  `outgoing_msg_id` int(255) NOT NULL,
-  `msg` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+    `msg_id` int(11) NOT NULL,
+    `incoming_msg_id` int(255) NOT NULL,
+    `outgoing_msg_id` int(255) NOT NULL,
+    `msg` varchar(1000) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
 -- --------------------------------------------------------
-
 --
 -- Table structure for table `users`
 --
+CREATE TABLE `users` (
+`user_id` int(11) NOT NULL,
+`unique_id` int(255) NOT NULL,
+`fname` varchar(255) NOT NULL,
+`lname` varchar(255) NOT NULL,
+`email` varchar(255) NOT NULL,
+`password` varchar(255) NOT NULL,
+`img` varchar(255) NOT NULL,
+`status` varchar(255) NOT NULL
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
