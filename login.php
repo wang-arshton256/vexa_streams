512b490e6dc6f5cc7cee264e37935bd934ba05b4
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 17:24:42 +0300

    updated some files

--- login.php
+++ login.php
@@ -1,16 +1,15 @@
-<?php
-
-session_start();
-if (isset($_SESSION['unique_id'])) {
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
     header("location: users.php");
-}
+  }
 ?>
 
-<?php include_once "header.php";?>
+<?php include_once "header.php"; ?>
 <body>
   <div class="wrapper">
     <section class="form login">
-      <header>Vexa Chat App</header>
+      <header>Realtime Chat App</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
@@ -29,7 +28,7 @@ if (isset($_SESSION['unique_id'])) {
       <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
     </section>
   </div>
-
+  
   <script src="javascript/pass-show-hide.js"></script>
   <script src="javascript/login.js"></script>
 
--- users.php
+++ users.php
@@ -1,27 +1,25 @@
-<?php
-
-session_start();
-include_once "php/config.php";
-if (!isset($_SESSION['unique_id'])) {
+<?php 
+  session_start();
+  include_once "php/config.php";
+  if(!isset($_SESSION['unique_id'])){
     header("location: login.php");
-}
+  }
 ?>
-<?php include_once "header.php";?>
+<?php include_once "header.php"; ?>
 <body>
   <div class="wrapper">
     <section class="users">
       <header>
         <div class="content">
-          <?php
-
-$sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
-if (mysqli_num_rows($sql) > 0) {
-    $row = mysqli_fetch_assoc($sql);
-}
-?>
+          <?php 
+            $sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+            if(mysqli_num_rows($sql) > 0){
+              $row = mysqli_fetch_assoc($sql);
+            }
+          ?>
           <img src="php/images/<?php echo $row['img']; ?>" alt="">
           <div class="details">
-            <span><?php echo $row['fname'] . " " . $row['lname']; ?></span>
+            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
             <p><?php echo $row['status']; ?></p>
           </div>
         </div>
@@ -33,7 +31,7 @@ if (mysqli_num_rows($sql) > 0) {
         <button><i class="fas fa-search"></i></button>
       </div>
       <div class="users-list">
-
+  
       </div>
     </section>
   </div>
