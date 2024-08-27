01f87296842e750b2ae0606b7c15c5bb99afe018
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 15:09:43 +0300

    querry users

--- users.php
+++ users.php
@@ -1,11 +1,27 @@
-<?php include_once "header.php"; ?>
+<?php
+
+session_start();
+include_once "php/config.php";
+if (!isset($_SESSION['unique_id'])) {
+    header("location: login.php");
+}
+?>
+<?php include_once "header.php";?>
 <body>
   <div class="wrapper">
     <section class="users">
       <header>
-        <div class="content"><img src="php/images/<?php echo $row['img']; ?>" alt="">
+        <div class="content">
+          <?php
+
+$sql = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}");
+if (mysqli_num_rows($sql) > 0) {
+    $row = mysqli_fetch_assoc($sql);
+}
+?>
+          <img src="php/images/<?php echo $row['img']; ?>" alt="">
           <div class="details">
-            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+            <span><?php echo $row['fname'] . " " . $row['lname']; ?></span>
             <p><?php echo $row['status']; ?></p>
           </div>
         </div>
@@ -17,7 +33,12 @@
         <button><i class="fas fa-search"></i></button>
       </div>
       <div class="users-list">
-  
+
       </div>
     </section>
-</div>
+  </div>
+
+  <script src="javascript/users.js"></script>
+
+</body>
+</html>
