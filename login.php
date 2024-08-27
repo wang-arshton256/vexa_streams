79cf2cd8c1b42fa3a6d56a58148c738353f3acbc
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 18:56:48 +0300

    updated login

--- login.php
+++ login.php
@@ -1,15 +1,16 @@
-<?php 
-  session_start();
-  if(isset($_SESSION['unique_id'])){
+<?php
+
+session_start();
+if (isset($_SESSION['unique_id'])) {
     header("location: users.php");
-  }
+}
 ?>
 
-<?php include_once "header.php"; ?>
+<?php include_once "header.php";?>
 <body>
   <div class="wrapper">
     <section class="form login">
-      <header>Realtime Chat App</header>
+      <header>Vexa Chat App</header>
       <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
         <div class="error-text"></div>
         <div class="field input">
@@ -28,7 +29,7 @@
       <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
     </section>
   </div>
-  
+
   <script src="javascript/pass-show-hide.js"></script>
   <script src="javascript/login.js"></script>
 
