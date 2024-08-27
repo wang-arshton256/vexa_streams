76e82c97716cad055b6d5142e59c882a085953a4
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 15:08:09 +0300

    updated user page

--- users.php
+++ users.php
@@ -3,4 +3,21 @@
   <div class="wrapper">
     <section class="users">
       <header>
-        <div class="content"></div>
+        <div class="content"><img src="php/images/<?php echo $row['img']; ?>" alt="">
+          <div class="details">
+            <span><?php echo $row['fname']. " " . $row['lname'] ?></span>
+            <p><?php echo $row['status']; ?></p>
+          </div>
+        </div>
+        <a href="php/logout.php?logout_id=<?php echo $row['unique_id']; ?>" class="logout">Logout</a>
+      </header>
+      <div class="search">
+        <span class="text">Select an user to start chat</span>
+        <input type="text" placeholder="Enter name to search...">
+        <button><i class="fas fa-search"></i></button>
+      </div>
+      <div class="users-list">
+  
+      </div>
+    </section>
+</div>
