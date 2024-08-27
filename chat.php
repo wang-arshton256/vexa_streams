a147f9f2d17de5e74550e3c6e43fbfede2761802
Wangutusi Arshton <wangarshton@gmail.com>
2024-08-27 15:12:04 +0300

    chat querry

--- chat.php
+++ chat.php
@@ -11,3 +11,12 @@ if (!isset($_SESSION['unique_id'])) {
   <div class="wrapper">
     <section class="chat-area">
       <header>
+<?php
+
+$user_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+$sql     = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$user_id}");
+if (mysqli_num_rows($sql) > 0) {
+    $row = mysqli_fetch_assoc($sql);
+} else {
+    header("location: users.php");
+}
